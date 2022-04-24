17862af97ea739b2a9e4f752e4ccf5a835f86170
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-24 18:50:55 +0100

    bug follow this one come

--- app.js
+++ app.js
@@ -11,7 +11,24 @@ function PageTransitions() {
             currentButton[0].className = currentButton[0].className.replace('active-btn', '');
             this.className += ' active-btn';
         })
-    }    
+    }   
+
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id){
+            sectionButtons.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
